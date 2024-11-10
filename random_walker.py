39885af8717d4ee10049073bc767bef25de03f2c
YuvalZippin <yuval.zippin@gmail.com>
2024-11-10 10:01:08 +0200

    Random Walker histogram
    his code simulates multiple random walks, each consisting of a specified number of jumps, and optionally visualises the results.

---
+++ random_walker.py
@@ -0,0 +1,26 @@
+# This code simulates multiple random walks, each consisting of a specified number of jumps, and optionally visualizes the results.
+#! to see the graph of multiple walks uncomment line 23
+#! to see the histogram of the experiments uncomment line 24
+
+import matplotlib.pyplot as plt
+import random
+
+JUMPS = 25
+EXPERIMENTS = 5
+
+def random_walk(jumps):
+  position = 0
+  track = [position]
+  for i in range(jumps):
+    position += random.choice([-1, 1])
+    track.append(position)
+  return track,position
+
+results = []
+for j in range (EXPERIMENTS):
+  results.append(random_walk(JUMPS))
+
+plt.plot(random_walk(JUMPS)[0])
+#plt.plot([result[0] for result in results],marker = 'o')
+#plt.hist([result[1] for result in results])
+plt.show()
